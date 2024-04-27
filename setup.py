bbea6ccc8fcb8770e72c483e752ab50d1b7629f0
Sahil <sahil.aggarwal.dev@gmail.com>
2024-04-28 03:29:06 +0530

    added webapp

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.1',
+    author= 'Sahil',
+    author_email= 'sahilaggarwal.it20@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
